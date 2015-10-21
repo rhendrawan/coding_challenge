572922c5fbfb2edbaf605af24fd43435e9d81511
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-10-20 19:02:58 -0700

    index.html and script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function reverse(string) {
+  var result = '';
+
+  for (var i = string.length - 1; i >= 0; i--) {
+    result += string.charAt(i);
+  }
+
+  return result;
+}
+
+console.log(reverse('hello'));
 
