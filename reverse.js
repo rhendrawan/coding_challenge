ac92f268163537f993cd38020d3c88bd1f245db7
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-10-20 18:55:42 -0700

    finish reverse.js

--- reverse.js
+++ reverse.js
@@ -1,3 +1,11 @@
-function reverse() {
+function reverse(string) {
+  var result = '';
 
+  for (var i = string.length - 1; i >= 0; i--) {
+    result += string.charAt(i);
+  }
+
+  return result;
 }
+
+console.log(reverse('hello'));
