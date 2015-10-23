cc07867cfb5d6a3294b133543dca28408890c7e5
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-10-22 19:43:57 -0700

    adds characterFrequency.js

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-function bubbleSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j+1]) {
-        var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-      }
-    }
+// Write a function that takes as its input a string and returns an array
+// of arrays as shown below sorted in descending order by frequency and
+// then by ascending order by character.
+function characterFrequency(string) {
+  var frequency = {};
+  var result = [];
+
+  for (var i = 0; i < string.length; i++) {
+    frequency[string[i]] = frequency[string[i]] || 0;
+    frequency[string[i]]++;
   }
 
-  return arr;
+  //push pairs into final result array
+  for (var prop in frequency) {
+    result.push([prop, frequency[prop]]);
+  }
+
+  //sort accordingly
+  result.sort(function(a, b) {
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] < b[1]) {
+      return 1;
+    } else if (a[0] < b[0]) {
+      return -1;
+    } else if (a[0] > b[0]) {
+      return 1;
+    }
+  });
+
+  return result;
+
 }
 
-console.log(bubbleSort([2, 8, 15, 10, 3, 6, -2, -8]));
+console.log(characterFrequency('mississippi'));   //[ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+console.log(characterFrequency('aaabbc'));    // [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+console.log(characterFrequency(''));    //[]
