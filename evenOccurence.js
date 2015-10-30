ebf1481e33332976f2c6fe6a837b38e81e3baee7
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-10-29 19:50:55 -0700

    adds evenOccurence.js

--- evenOccurence.js
+++ evenOccurence.js
@@ -1,1 +1,23 @@
+// Find the first item that occurs an even number of times in an array.
+// Remember to handle multiple even-occurrence items and return the first one.
+// Return null if there are no even-occurrence items.
+function evenOccurence(arr) {
+  var letters = {};
 
+  for (var i = 0; i < arr.length; i++) {
+    var letter = arr[i];
+    letters[letter] = letters[letter] || 0;
+    letters[letter]++;
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    if (letters[arr[i]] % 2 === 0) {
+      return arr[i];
+    }
+  }
+
+  return null;
+}
+
+console.log(evenOccurence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]));
+console.log(evenOccurence([ "cat", "dog", "dig", "cat" ]));
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function commonCharactrers(string1, string2) {
-  var result = '';
+// Find the first item that occurs an even number of times in an array.
+// Remember to handle multiple even-occurrence items and return the first one.
+// Return null if there are no even-occurrence items.
+function evenOccurence(arr) {
+  var letters = {};
 
-  for (var i = 0; i < string1.length; i++) {
-    if (string2.indexOf(string1.charAt(i)) >= 0) {
-      if (result.indexOf(string1.charAt(i)) < 0) {
-        result += string1.charAt(i);
-      }
+  for (var i = 0; i < arr.length; i++) {
+    var letter = arr[i];
+    letters[letter] = letters[letter] || 0;
+    letters[letter]++;
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    if (letters[arr[i]] % 2 === 0) {
+      return arr[i];
     }
   }
 
-  return result;
+  return null;
 }
 
-console.log(commonCharactrers('acexivou', 'aegihobu'));   //'aeiou'
+console.log(evenOccurence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]));
+console.log(evenOccurence([ "cat", "dog", "dig", "cat" ]));
