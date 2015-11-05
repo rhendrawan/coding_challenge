767a06925f8afdcd9df608fff3db19da16f4038f
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-11-04 18:23:19 -0800

    adds longestRun.js

--- longestRun.js
+++ longestRun.js
@@ -1,1 +1,37 @@
+// Write a function that, given a string, finds the longest run of identical characters
+// and returns an array containing the start and end indices of that run.
+// If there are two runs of equal length, return the first one. Return [0,0] for no runs.
+var longestRun = function(string) {
+  var count = 0;
+  var longestCount = 0;
+  var currentStart = 0;
+  var currentEnd = 0;
+  var start = 0;
+  var end = 0;
 
+  for (var i = 0; i < string.length; i++) {
+    // if new character
+    if (string[i] !== string[i-1]) {
+      count = 1;
+      currentStart = i;
+      currentEnd = i;
+    } else {
+    // same character as previous
+      count++;
+      currentEnd++;
+      if (longestCount < count) {
+        longestCount = count;
+        start = currentStart;
+        end = currentEnd;
+      }
+    }
+  }
+
+  return [start, end];
+};
+
+console.log(longestRun("abbbcc"));
+console.log(longestRun("aabbc"));
+console.log(longestRun(""));
+console.log(longestRun("mississippi"));
+console.log(longestRun("abcdefgh"));
--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-var insertionSort = function(arr) {
-  var temp;
+// Write a function that, given a string, finds the longest run of identical characters
+// and returns an array containing the start and end indices of that run.
+// If there are two runs of equal length, return the first one. Return [0,0] for no runs.
+var longestRun = function(string) {
+  var count = 0;
+  var longestCount = 0;
+  var currentStart = 0;
+  var currentEnd = 0;
+  var start = 0;
+  var end = 0;
 
-  for (var i = 0; i < arr.length; i++) {
-    temp = arr[i];
-
-    for (var j = i-1; j >= 0; j--) {
-      if (arr[j] > temp) {
-        arr[j+1] = arr[j];
-        arr[j] = temp;
+  for (var i = 0; i < string.length; i++) {
+    // if new character
+    if (string[i] !== string[i-1]) {
+      count = 1;
+      currentStart = i;
+      currentEnd = i;
+    } else {
+    // same character as previous
+      count++;
+      currentEnd++;
+      if (longestCount < count) {
+        longestCount = count;
+        start = currentStart;
+        end = currentEnd;
       }
     }
   }
 
-  return arr;
+  return [start, end];
 };
 
-console.log(insertionSort([ 3, 1, 2 ]));
-console.log(insertionSort([ 1, 0, 100, 42 ]));
+console.log(longestRun("abbbcc"));
+console.log(longestRun("aabbc"));
+console.log(longestRun(""));
+console.log(longestRun("mississippi"));
+console.log(longestRun("abcdefgh"));
