196404c7bebbf8b45619ac459edc8bd6519b95ed
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-12-01 21:05:57 -0800

    adds translateRomanNumeral.js

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-var rockPaperScissorPermutation = function(count) {
-  var result = [];
-  var plays = ['r', 'p', 's'];
+function translateRomanNumeral(roman) {
+  var DIGIT_VALUES = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
 
-  function recurse(string) {
-    if (string.length === count) {
-      result.push(string);
-      return;
+  var result = 0;
+
+  for (var i = 0; i < roman.length; i++) {
+    var curNum = DIGIT_VALUES[roman[i]];
+    var nextNum = DIGIT_VALUES[roman[i+1]];
+
+    if (curNum === undefined) {
+      return "null";
     }
 
-    for (var i = 0; i < plays.length; i++) {
-      recurse(string + plays[i]);
+    if (nextNum && curNum < nextNum) {
+      result -= curNum;
+    } else {
+      result += curNum;
     }
   }
 
-  recurse('');
   return result;
-};
+}
 
-
-console.log(rockPaperScissorPermutation(2));
-console.log(rockPaperScissorPermutation(3));
+console.log(translateRomanNumeral("LX"));
+console.log(translateRomanNumeral("IV"));
+console.log(translateRomanNumeral("horse"));
+console.log(translateRomanNumeral(""));
--- translateRomanNumeral.js
+++ translateRomanNumeral.js
@@ -1,1 +1,37 @@
+// Given a roman numeral as input, write a function that converts
+// the roman numeral to a number and outputs it.
+function translateRomanNumeral(roman) {
+  var DIGIT_VALUES = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
 
+  var result = 0;
+
+  for (var i = 0; i < roman.length; i++) {
+    var curNum = DIGIT_VALUES[roman[i]];
+    var nextNum = DIGIT_VALUES[roman[i+1]];
+
+    if (curNum === undefined) {
+      return "null";
+    }
+
+    if (nextNum && curNum < nextNum) {
+      result -= curNum;
+    } else {
+      result += curNum;
+    }
+  }
+
+  return result;
+}
+
+console.log(translateRomanNumeral("LX"));
+console.log(translateRomanNumeral("IV"));
+console.log(translateRomanNumeral("horse"));
+console.log(translateRomanNumeral(""));
