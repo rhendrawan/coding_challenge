0301c371d20de3598a52244367382208ce94d2a9
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-12-21 15:12:04 -0800

    adds sumArray.js

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-var spiralTraversal = function(matrix){
-  var results = [];
-  var minRow = 0;
-  var maxRow = matrix.length-1;
-  var minCol = 0;
-  var maxCol = matrix[0].length-1;
+// Given an array of numbers, calculate the greatest contiguous sum of numbers in it.
+// A single array item will count as a contiguous sum.
+function sumArray(array) {
+  maxSum = -Infinity;
 
-  while (minRow <= maxRow && minCol <= maxCol) {
-    for (var i = minCol; i <= maxCol; i++) {
-      results.push(matrix[minRow][i]);
-    }
-    minRow++;
-    for (var j = minRow; j <= maxRow; j++) {
-      results.push(matrix[j][maxCol]);
-    }
-    maxCol--;
-    if (minRow <= maxRow) {
-      for (var k = maxCol; k >= minCol; k--) {
-        results.push(matrix[maxRow][k]);
+  for (var i = 0; i < array.length; i++) {
+    var currSum = 0;
+
+    for (var j = i; j < array.length; j++) {
+      currSum += array[j];
+      if (currSum > maxSum) {
+        maxSum = currSum;
       }
-      maxRow--;
-    }
-    if (minCol <= maxCol) {
-      for (var m = maxRow; m >= minRow; m--) {
-        results.push(matrix[m][minCol]);
-      }
-      minCol++;
     }
   }
-  return results;
-};
 
-console.log(
-  spiralTraversal(
-    [ [ 1, 2, 3, 4, 5 ],
-      [ 6, 7, 8, 9, 10 ],
-      [ 11, 12, 13, 14, 15 ],
-      [ 16, 17, 18, 19, 20 ],
-      [ 21, 22, 23, 24, 25 ]
-    ]
-  )
-);
+  return maxSum;
+}
+
+console.log(sumArray([ 1, 2, 3 ]));   //6
+console.log(sumArray([ 4, -1, 5 ]));   //8
+console.log(sumArray([ 10, -11, 11 ]));   //11
+console.log(sumArray([ 1, 2, 3, -6, 4, 5, 6 ]));   //15
--- sumArray.js
+++ sumArray.js
@@ -1,1 +1,23 @@
+// Given an array of numbers, calculate the greatest contiguous sum of numbers in it.
+// A single array item will count as a contiguous sum.
+function sumArray(array) {
+  maxSum = -Infinity;
 
+  for (var i = 0; i < array.length; i++) {
+    var currSum = 0;
+
+    for (var j = i; j < array.length; j++) {
+      currSum += array[j];
+      if (currSum > maxSum) {
+        maxSum = currSum;
+      }
+    }
+  }
+
+  return maxSum;
+}
+
+console.log(sumArray([ 1, 2, 3 ]));   //6
+console.log(sumArray([ 4, -1, 5 ]));   //8
+console.log(sumArray([ 10, -11, 11 ]));   //11
+console.log(sumArray([ 1, 2, 3, -6, 4, 5, 6 ]));   //15
